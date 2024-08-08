1af777dda8bd6e0c615718840ea93c0a51ab84e5
Ganesh Dutt <gdutt2304@gmail.com>
2024-08-08 19:24:11 +0530

    Loop

---
+++ src/Decision_Making_Statement/class2.java
@@ -0,0 +1,30 @@
+package Decision_Making_Statement;
+
+public class class2 {
+    public static void main(String[] args) {
+
+      for(int i = 0; i < 10; i++) {
+          System.out.println("Ganesh : " + i );
+      }
+
+
+//      int i = 0;
+//      while(i <= 10){
+//          System.out.println(i);
+//          i++;
+//      }
+
+int i = 0;
+      do{
+          System.out.println(i);
+          i++;
+      }while(i < 10);
+
+
+      int []arr = {1,2,3,5,2,3,4,5};
+      for(int a : arr){
+          System.out.print(a);
+      }
+
+    }
+}
