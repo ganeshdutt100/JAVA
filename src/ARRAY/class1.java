5d20f8ceb6cec54419be7b5b114a396adcbb8933
Ganesh Dutt <gdutt2304@gmail.com>
2024-08-12 19:01:01 +0530

    Test

---
+++ src/ARRAY/class1.java
@@ -0,0 +1,10 @@
+package ARRAY;
+
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println("Array");
+
+        int a[] = {10,45,87,98,56};
+        System.out.println(a[3]);
+    }
+}
